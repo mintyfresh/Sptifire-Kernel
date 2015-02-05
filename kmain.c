3e58cf97770114793b3f1dc30083bed05903ecda
Mihail.K <Mihail.K@fimu.tk>
2015-02-05 14:02:14 -0500

    Testing string functions.

--- kmain.c
+++ kmain.c
@@ -1,12 +1,18 @@
 
 # include "kernel.h"
 # include "kscreen.h"
+# include "kstring.h"
 
 void kmain(int magic, void *mboot) {
 	// Validate multiboot magic
 	assert(magic == 0x2BADB002);
 
 	kprintf("Done.\n");
+
+	char *test = "Hello!";
+	char buffer[64];
+
+	kprintf("%s : %s.\n", test, strcpy(buffer, test));
 
 	((void)mboot);
 
